aec5627ff3a0f06cdfe44a34915a2989952622a2
Kestas0302 <kestutis.margelis1992@gmail.com>
2022-04-26 21:11:19 +0300

    comments in javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+// Vienos linijos komentaras
+/* 
+    Sveiki cia yra 
+    Multi line komentaras
+
+*/
+
+// Sukuriam kintamuosius
+let myName; 
+let age;
+//  Priskiriam kintamiesiems reiksmes
+myName = "Kestutis"; 
+age = 30;
