e2f281a3d2b661716a79b776f7400ad101b1a5ac
Kestas0302 <kestutis.margelis1992@gmail.com>
2022-06-14 22:27:21 +0300

    Add post example

--- 25paskaita/script.js
+++ 25paskaita/script.js
@@ -4,7 +4,7 @@ console.log(" ");
 
 const renderTodo = (todo) => {
     console.log(todo);
-    const {id, completed, title} = todo;
+    const {completed, title} = todo;
     // const id = todo.id;
     // const completed = todo.completed;
     // const title = todo.title;
@@ -27,15 +27,52 @@ document.querySelector(".todo-container").prepend(todoEl);
 fetch("https://jsonplaceholder.typicode.com/todos")
 .then(resp => resp.json())
 .then((responce) => {
-    const todos = responce.slice(0, 10);
+    const todos = responce.slice(0, 5);
 
     // renderTodo(todos[0]);
-    responce.forEach(todo => renderTodo(todo));
+    todos.forEach(todo => renderTodo(todo));
 })
 .catch((error) => {
     console.error(error);
 });
 
+const handleAddTodo = () => {
+    const todoText = document.querySelector('input[name="todo-text"]');
+    // console.log(todoText.value);
+    // console.log("Paspaudziau add mygtuka");
+    const newTodo = {title: todoText.value, completed: false};
+
+    const params = {
+        method: "POST",
+        body: JSON.stringify(newTodo),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    };
+
+
+fetch("https://jsonplaceholder.typicode.com/todos", params)
+    .then(resp => resp.json())
+    .then(responce =>{
+     renderTodo(responce);
+     todoText.value = "";
+     console.log(responce)
+    })
+.catch((error) => {
+    console.error(error);
+});
+    // renderTodo(newTodo);
+    // todoText.value = "";
+};
+
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", handleAddTodo);
+
+// const handleAddTodo = (e) => {
+//     const todoText = document.querySelector('input[name="todo-text"]');
+//     console.log(todoText);
+//     console.log("Paspaudziau add mygtuka");
+// }
 // Sukurti addEventListener su handlerliu onClick
 
 // Nuspaudus ant elemento pakeisti todoStatus klasę (jeigu klasė yra done, pakeisti į undone. Jeigu klasė yra undone, pakeisti į done)
---
+++ 26paskaita/script.js
@@ -0,0 +1,2 @@
+console.log("26 paskaita");
+console.log(" ");
